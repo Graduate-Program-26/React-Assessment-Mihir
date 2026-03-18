5f6f1fb6769159e5ebf81325db55f8c91c0cc6fb
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-03-18 09:05:55 +0200

    updated styling for login page and temporary styling for logout

--- app/ui/login/SignIn.tsx
+++ app/ui/login/SignIn.tsx
@@ -1,14 +1,42 @@
 import { signIn } from "@/auth"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { FaGithub } from "react-icons/fa";
 
 export default function SignIn() {
     return (
-        <form
-            action={async () => {
-                "use server"
-                await signIn("github")
-            }}
-        >
-            <button type="submit">Signin with GitHub</button>
-        </form>
+        <>
+            <div className="flex min-h-screen items-center justify-center">
+                <Card className="w-full max-w-sm">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Click on the button below to sign in
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form
+                            action={async () => {
+                                "use server"
+                                await signIn("github")
+                            }}
+                        >
+                            <div className="flex justify-self-center">
+                                <FaGithub size={300} />
+                            </div>
+                            <Button variant="outline" type="submit" className="w-full mt-4">
+                                Sign in with Github
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     )
-} 
+}
