d857ea01b4bc740498fe82287471180f1b3cee7a
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-03-24 15:46:12 +0200

    added a proper landing page and updated routing in sign out to take you there

--- app/ui/login/SignOut.tsx
+++ app/ui/login/SignOut.tsx
@@ -6,7 +6,7 @@ export default function SignOut() {
         <form
             action={async () => {
                 "use server"
-                await signOut({ redirectTo: "/login" })
+                await signOut({ redirectTo: "/" })
             }}
         >
             <button
