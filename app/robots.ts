726611a80456435606bd33b3945531dd679ca20c
Mihir-Arjun-dvt <MArjun@dvtsoftware.com>
2026-03-26 13:01:35 +0200

    fixed URL issue

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://yourdomain.com/sitemap.xml',
+        sitemap: 'https://react-assessment-mihir-2gi9736oz-mihir-arjun-dvts-projects.vercel.app',
     }
 }
